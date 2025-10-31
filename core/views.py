7007b7f0f1007c55d829599fb36ec7c4a3ed1723
wa <alexislacasse1@gmail.com>
2025-10-31 15:58:05 -0400

    backend python

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def assistant_epicerie(request):
+    return render(request, 'core/Assistant Épicerie.html')
+
+def optimiseur_rabais(request):
+    return render(request, 'core/Optimiseur Rabais.html')from django.shortcuts import render
+
+# Create your views here.
